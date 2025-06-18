20d1a947ce3d1007914a031a61f64a81a41cbcd0
bon-nina <215746632+bon-nina@users.noreply.github.com>
2025-06-18 20:20:25 +0200

    Added function to play 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,9 +1,6 @@
 
-let humanScore = 0;
-
-let computerScore = 0;
-
-function getComputerChoice () { //gets randomized choice from computer
+//gets randomized choice from computer
+function getComputerChoice () { 
     let randomNumber;
     let choice;
     randomNumber = Math.random()*100;
@@ -22,35 +19,53 @@ function getComputerChoice () { //gets randomized choice from computer
     return choice;
 }
 
-function getHumanChoice () { //asks player to input their choice into prompt
+
+//asks player to input their choice into prompt
+function getHumanChoice () { 
     let choice;
     choice = prompt("Enter your choice: Rock, Paper or Scissor: ");
     return choice;
 }
 
-function playRound (humanChoice,computerChoice) { //compares human and computer choice to decide winner
 
-    humanChoice = humanChoice.toLowerCase(); //makes player prompt input case-insensitive
+//runs through playRound five times 
+function playGame() {
 
-    //Win and Loss conditions and update score counter
-    if (( (humanChoice == "rock") && (computerChoice == "scissor") ) || 
-        ( (humanChoice == "scissor") && (computerChoice == "paper") ) ||
-        ( (humanChoice == "paper") && (computerChoice == "rock") ) ) {
-            console.log("Win! " + humanChoice.toUpperCase() + " beats " + computerChoice.toUpperCase() + " !");
-            humanScore += 1;
-        }
+    let humanScore = 0;
+    let computerScore = 0;
 
-    else if (( (humanChoice == "rock") && (computerChoice == "paper") ) ||
-        ( (humanChoice == "scissor") && (computerChoice == "rock") ) ||
-        ( (humanChoice == "paper") && (computerChoice == "scissor") ) ) {
-            console.log("Loss! " + computerChoice.toUpperCase() + " beats " + humanChoice.toUpperCase() + " !");
-            computerScore += 1;
-        }
-
-    else {
-        console.log("Tie! Try again");
+    for (let i= 0; i < 5; i++) {
+        //starts round and gives player and computer choice to playRound()
+        playRound(getHumanChoice(),getComputerChoice()); 
     }
+
+    //compares human and computer choice to decide winner
+    function playRound (humanChoice,computerChoice) { 
+
+        //makes player prompt input case-insensitive
+        humanChoice = humanChoice.toLowerCase();
+
+        //Win and Loss conditions and update score counter
+        if (( (humanChoice == "rock") && (computerChoice == "scissor") ) || 
+            ( (humanChoice == "scissor") && (computerChoice == "paper") ) ||
+            ( (humanChoice == "paper") && (computerChoice == "rock") ) ) {
+                console.log("Win! " + humanChoice.toUpperCase() + " beats " + computerChoice.toUpperCase() + " !");
+                humanScore += 1;
+            }
+
+        else if (( (humanChoice == "rock") && (computerChoice == "paper") ) ||
+            ( (humanChoice == "scissor") && (computerChoice == "rock") ) ||
+            ( (humanChoice == "paper") && (computerChoice == "scissor") ) ) {
+                console.log("Loss! " + computerChoice.toUpperCase() + " beats " + humanChoice.toUpperCase() + " !");
+                computerScore += 1;
+            }
+
+        else {
+            console.log("Tie! Try again");
+            }
+
+    } 
 
 }
 
-playRound(getHumanChoice(),getComputerChoice()); //starts round and gives player and computer choice to playRound()
+playGame();
